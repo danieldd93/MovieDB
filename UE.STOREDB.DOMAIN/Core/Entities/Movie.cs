f416d34a0c26693ea99f7fed326bb233cda25c23
danieldd93 <danieldd.93@gmail.com>
2024-05-18 16:06:41 -0500

    Scaffolding

---
+++ UE.STOREDB.DOMAIN/Core/Entities/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieDB.DOMAIN.Core.Entities;
+
+public partial class Movie
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Genre { get; set; }
+
+    public int ReleaseYear { get; set; }
+
+}
---
+++ UE.STOREDB.DOMAIN/Infrastructure/Data/MovieDbueContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MovieDB.DOMAIN.Core.Entities;
+
+namespace MovieDB.DOMAIN.Infrastructure.Data;
+
+public partial class MovieDbueContext : DbContext
+{
+    public MovieDbueContext()
+    {
+
+    }
+
+    public MovieDbueContext(DbContextOptions<MovieDbueContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Movie> Movie { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=Parcial20240120200113;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
